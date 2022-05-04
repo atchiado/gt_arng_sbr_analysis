3c5a38135c47f9bbcafd0332f0bcfa41bfc531a5
Anthony Chiado <anthonychiado97@gmail.com>
2022-05-04 12:49:36 -0400

    Intitial commit.

---
+++ arng_sbr_analysis.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+
+
+# Load Data ------------------
+sbr <- read.csv("~/Desktop/work/data/r/gt_arng_sbr_analysis/arng_sb_data.csv")
+
+
+# Clean Data --------------------
+as.factor(sbr$SB_Recruiter)
+
+
+# Data Viz --------------------
